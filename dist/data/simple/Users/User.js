dbde7be1f39a6cdd258ff335064ba5ec1488aaee
Arthur Tkachenko <atherdon@gmail.com>
2019-01-31 21:18:00 +0200

    +++

--- dist/data/simple/Users/User.js
+++ dist/data/simple/Users/User.js
@@ -9,31 +9,6 @@ let attributes  = [
 ];
 
 
-const get = () => {
-
-    var data     = [
-      {
-        name: 'john',
-        email: 'john.doe@ibm.com',
-        password: 'john1',
-      },
-      {
-        name: 'jane',
-        email: 'jane.doe@ibm.com',
-        password: 'jane1',
-      },
-      {
-        name: 'admin',
-        email: 'admin@ibm.com',
-        password: 'admin',
-      }
-     ];
-
-  	return data;
-
-};
-
-
 // @TODO this is a duplicate
 function assignAdmin(options, admin_id){
   let server
